96020f32f0ea2149b6bbaadfaf5aa0f00f512b7c
Peter Lieverdink <me@cafuego.net>
2010-01-12 10:57:06 +1100

    [project @ me@cafuego.net-20100111235706-58drw11t7jvhle1s] Added option to colourise the submit button

--- theme/sessions-form.tpl.php
+++ theme/sessions-form.tpl.php
@@ -4,13 +4,13 @@
   *  - $submit: Submit button value.
   *  - $action: Form action url.
   *  - $hidden: Array of hidden fields.
+  *  - $colour: Colour of the button.
   */
 ?>
 
-<form action="<?php print $action; ?>" method="GET">
+<form action="<?php print $action; ?>" method="POST">
 <?php foreach ($hidden as $name => $value): ?>
   <input type="hidden" name="<?php print $name; ?>" value="<?php print $value; ?>" />
 <?php endforeach; ?>
-  <input class="form-submit" type="submit" value="<?php print $submit; ?>" />
+  <input class="form-submit <?php print $colour; ?>" type="submit" value="<?php print $submit; ?>" />
 </form>
-
