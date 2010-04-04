b57a93d25642bdfafc07b9de5829e3393eba2531
Peter Lieverdink <me@cafuego.net>
2010-04-04 00:23:22 +0000

    [project @ peter@cc.com.au-20100404002322-caktc44htf6u5mm7] New ical icon.

---
+++ theme/sessions-print.tpl.php
@@ -0,0 +1,51 @@
+<?php
+ /**
+  * Variables:
+  *  - $account: A valid username string.
+  *  - $content: A keyed array containing data to be tabulated.
+  *    'header' - The table headers.
+  *    'rows'   - The table rows.
+  */
+
+  foreach ($content['rows'] as $idx => $row) {
+    unset($content['rows'][$idx]['date_from']);
+    unset($content['rows'][$idx]['date_to']);
+    unset($content['rows'][$idx]['nid']);
+    unset($content['rows'][$idx]['uuid']);
+  }
+
+  $table = theme('table', $content['header'], $content['rows'], array('cellpadding' => 8, 'cellspacing' => 0));
+?>
+
+<html>
+<head>
+  <title>My Program for <?php print $username; ?></title>
+<style type="text/css">
+  table tr th {
+    color: #fff;
+    background: #333;
+  }
+  table tr.even {
+    background: #ccc;
+  }
+  table td {
+    align: left;
+    verical-align: top;
+  }
+  h2, h3 {
+    font-family: sans-serif;
+  }
+  h3 {
+    margin-bottom: 0px;
+    margin-top: 8px;
+  }
+</style>
+</head>
+<body>
+
+  <h2>MyACEC Program for <?php print $username; ?></h2>
+
+  <?php print $table; ?>
+
+</body>
+</html>
